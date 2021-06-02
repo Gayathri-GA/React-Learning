2e0a310161f052f9c522bf7b519867189884874b
Gayathri Devi Srinivasan <gayathrid@geekyants.com>
2021-06-03 05:27:05 +0530

    Concepts project added

---
+++ concepts/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UserGreeting from './concepts/conditional_rendering';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserGreeting />
+      {/* <ParentComponent /> */}
+      {/* <EventBinding /> */}
+    </div>
+  );
+}
+
+export default App;
