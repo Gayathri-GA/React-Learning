02ae041b9b94475f60728e112a21fdd4943f0677
gayathrid <gayathrid@geekyants.com>
2021-06-18 15:52:15 +0530

    React fragments , componenet mounting and updating added

---
+++ intermediate_concepts/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import FragementDemo from './fragments';
+// import ParentMounting from './componentMounting/parentmount';
+function App() {
+  return (
+    <div className="App">
+      {/* <ParentMounting /> */}
+      <FragementDemo />
+    </div>
+  );
+}
+
+export default App;
